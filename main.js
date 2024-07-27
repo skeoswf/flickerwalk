1d2b67713e5165281b0b510a3d1f704e66e93dd8
Skeo <skeo051600@gmail.com>
2024-07-26 23:09:27 -0500

    get background image to work on start button click

---
+++ main.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('big-ass-start-button');
+
+startButton.addEventListener('click', () => {
+  document.body.style.backgroundImage = 'url(./background2.png)';
+});
