453c92b237b6cf059193ea0a77166296d2acbec5
Skeo <skeo051600@gmail.com>
2024-07-26 23:18:33 -0500

    Add working background

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const phaseOneElements = document.getElementsByClassName('phase-one');
 const startButton = document.getElementById('big-ass-start-button');
 
 startButton.addEventListener('click', () => {
   document.body.style.backgroundImage = 'url(./background2.png)';
+});
+
+phaseOneElements.addEventListener('click', () => {
+  console.log('clicked');
 });
