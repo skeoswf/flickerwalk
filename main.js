93d967b86354c5b4a8bc99f1af906d0c64b6a880
Skeo <skeo051600@gmail.com>
2024-07-26 23:31:43 -0500

    remove divs on click

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const startButton = document.getElementById('big-ass-start-button');
 
+const header = document.getElementById('header-text');
+const aboutButton = document.getElementById('about-button');
+
 startButton.addEventListener('click', () => {
+  header.remove();
+  aboutButton.remove();
+  startButton.remove();
   document.body.style.backgroundImage = 'url(./background2.png)';
+  document.body.style.backgroundSize = 'cover';
 });
